738da4c73620bc184344f56f2cc207b3f2a0cbe8
Igor Chudov <nir@altlinux.org>
2020-11-23 17:04:05 +0400

    Disk partitioning scheme changed

--- metadata/9-server/vm-profile.scm
+++ metadata/9-server/vm-profile.scm
@@ -2,6 +2,6 @@
   (title . "Setup for workstation")
   (action . trivial)
   (actiondata 
-               ("/" (size 20971520 . 20971520) (fsim . "Ext2/3") (methods plain))
-               ("/var" (size 1024000 . #t) (fsim . "Ext2/3") (methods  plain))))
-               )
+               ("/" (size 20971520 . 20971520 ) (fsim . "Ext2/3") (methods plain))
+               ;("/var" (size 1024000 . #t) (fsim . "Ext2/3") (methods  plain))
+               )))
